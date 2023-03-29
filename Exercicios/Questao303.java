eb2913c72922588675af68296fba4cf594324784
Eduarda Miranda <113375327+mirandaedu@users.noreply.github.com>
2023-03-29 16:51:18 -0300

    Questao303

--- Exercicios/Questao303.java
+++ Exercicios/Questao303.java
@@ -5,9 +5,10 @@ import java.util.*;
 public class Questao303 {
     public static void main(String [] args){
         int i, mult;
-        for(i=1; i<=500;){
-            mult = i*5;
-           System.out.println("");
+        for(i=1; i<=500; i++){
+            if(i%5==0){
+                 System.out.println(i);     
+            }
         }    
     }    
 }
