5fa04a05a0d949138988147ddf597ad6c1d1bbd1
Eduarda Miranda <113375327+mirandaedu@users.noreply.github.com>
2023-04-02 16:23:25 -0400

    Create Questao110.java

---
+++ Exercicios/Questao110.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+ public class Questao110{
+
+   public static void main(String args[]){ 
+
+     int a, fat;
+
+     a = 6;
+
+     
+
+     fat = (a*(a-1)*(a-2)*(a-3)*(a-4)*(a-5));
+
+         
+
+    System.out.println("O fatorial de 6 é igual a "+ fat);
+
+   }
+
+}
