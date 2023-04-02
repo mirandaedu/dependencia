35bb6717d8de4db263e1d026dde68603c067c3be
Eduarda Miranda <113375327+mirandaedu@users.noreply.github.com>
2023-04-02 16:21:16 -0400

    Create Questao107.java

---
+++ Exercicios/Questao107.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Questao107{
+
+  public static void main(String[] args){
+
+    Scanner scr = new Scanner(System.in);
+
+      int n, nquad;
+
+        System.out.print("Informe um número: ");
+
+        n = scr.nextInt();
+
+        nquad = n*n;
+
+     System.out.println("O quadrado deste número é: "+nquad);
+
+  } 
+
+}
