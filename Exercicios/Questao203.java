ae846281bb293a67508baa7c6c90cd77cb309753
Eduarda Miranda <113375327+mirandaedu@users.noreply.github.com>
2023-04-02 23:40:26 -0400

    Update Questao203.java

--- Exercicios/Questao203.java
+++ Exercicios/Questao203.java
@@ -1,26 +1,55 @@
-package Exercicios;
+
+
+import java.util.*;
 
 import java.util.Scanner;
 
+public class Questao203{
 
-public class Questao203 {
-    public static void main(String[] args){
-    Scanner scanner = new Scanner(System.in);
-    int idade = 0;
-    
-    
-    
-    
-    System.out.println("Informe sua idade: ");
-    idade=scanner.nextInt();
-    switch(sexo){
-        case femino:1;
-                break;
-        
-        
-    }
-    if((idade>=65)||(idade>=60)){
-        
-    }  
- }
+   public static void main(String args[]){ 
+
+     Scanner sc = new Scanner(System.in);
+
+     int sexo, idade;
+
+ 
+
+     System.out.println("Informe seu sexo: \n1 para Mulher e 2 para Homem");
+
+     sexo = sc.nextInt();
+
+     
+
+     System.out.println("Agora, Informe sua idade: ");
+
+     idade = sc.nextInt();
+
+     
+
+     if(sexo==1 && idade>=60){
+
+       System.out.print("\nVocê é mulher e pode se aposentar ");
+
+     }if(idade<60){
+
+         System.out.print("\nVocê não tem idade suficiente para se aposentar");
+
+        }
+
+       else{
+
+       if(sexo==2 && idade>=65){
+
+         System.out.print("\nVocê é homem e pode se aposentar ");
+
+        }if(idade<65){
+
+         System.out.print("\nVocê não tem idade suficiente para se aposentar");
+
+         }
+
+       }
+
+  }  
+
 }
