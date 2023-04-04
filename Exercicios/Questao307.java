a013d1508d61c27238ed013346f6b62c10d4125c
Eduarda Miranda <113375327+mirandaedu@users.noreply.github.com>
2023-04-04 19:06:33 -0400

    Create Questao307.java

---
+++ Exercicios/Questao307.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Questao307{
+
+  public static void main(String args[]){ 
+
+    int i;
+
+    
+
+    for(i=1; i<200; i++){
+
+      if(i%4==0){
+
+        System.out.println(i);
+
+      }
+
+    }
+
+  }
+
+}
