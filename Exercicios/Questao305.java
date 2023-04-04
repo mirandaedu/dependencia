ba3f5643c827f074bd12b757a2254de0b960a008
Eduarda Miranda <113375327+mirandaedu@users.noreply.github.com>
2023-04-04 18:17:47 -0400

    Create Questao305.java

---
+++ Exercicios/Questao305.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Questao305{
+
+  public static void main(String args[]){ 
+
+    int i;
+
+    for(i=1; i<=20; i++){
+
+      if(i%2==1){
+
+        System.out.println(i);
+
+      }
+
+    }
+
+  }
+
+}
