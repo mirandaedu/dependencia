0b50cc85231eb05dc6076787a6b7a388d4a0d4b2
Eduarda Miranda <113375327+mirandaedu@users.noreply.github.com>
2023-04-26 16:04:16 -0300

    bla

---
+++ Aulinha POO/src/aulinha/poo/Ponto.java
@@ -0,0 +1,20 @@
+package aulinha.poo;
+
+
+public class Ponto {
+   private int x;
+   private int y;
+    
+   public void setPosicao(int arg1, int arg2){
+      x = arg1;
+      y = arg2;
+   }
+    
+   public int getX(){ 
+        return x;
+   }
+    
+   public int getY(){
+        return y;
+   }
+}
