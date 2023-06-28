2941c50757a3187ad06e3d606d5f28567acca49e
Eduarda Miranda <113375327+mirandaedu@users.noreply.github.com>
2023-06-28 14:23:55 -0300

    Create ArrayList.java

---
+++ Exercicios/ArrayList.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class ExemploArray {
+    public static void main(String []args){
+        
+        ArrayList<String> clientes = new ArrayList<String>();
+        
+        clientes.add("Juan"); //Adiciona pessoa
+        clientes.add("João");
+        clientes.remove(1); //Remove uma posição
+        clientes.clear(); //Remove todas as posições
+        
+        System.out.println(clientes.toString()); //Mostra todo o conteúdo
+        System.out.println(clientes.get(1)); //Retorna valor da posição 1
+        System.out.println(clientes.isEmpty()); //Vai testar se o Array esta vazio ou não e vai retornar um valor boolean
+        System.out.println(clientes.contains("João")); //Vai testar se Joao esta dentro do Array 
+        System.out.println(clientes.size()); //Retorna o valor do ArrayList
+        System.out.println(clientes.indexOf("Juan")); //Retorna a posição de Juan     
+    } 
+}
